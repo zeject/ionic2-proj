b013f5b1a821ee84c2af08cc6be89c047477b125
zeject <424049902@qq.com>
2016-12-13 21:34:29 +0800

    remove import

--- src/directive/scroll.directive.ts
+++ src/directive/scroll.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, HostListener, ElementRef, Input, EventEmitter, Output } from '@angular/core';
+import { Directive, HostListener, ElementRef, EventEmitter, Output } from '@angular/core';
 
 @Directive({
     selector: '[scrollChange]'
